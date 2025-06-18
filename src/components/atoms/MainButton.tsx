30a9a26c7a24b6a9ac8188efd0f847001a2c1c24
mohammadrth11 <mohammad.rth11@gmail.com>
2025-06-18 11:34:46 +0400

    Build Responsive Hero Section

--- src/components/atoms/MainButton.tsx
+++ src/components/atoms/MainButton.tsx
@@ -10,7 +10,7 @@ export default function MainButton({ children, onClick, icon: Icon }: Props) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center gap-2 rounded-md px-5 py-2 text-white font-bold text-lg bg-gradient-to-r from-green-400 to-green-600 hover:from-green-500 hover:to-green-700 transition"
+      className="flex items-center justify-center gap-2 rounded-md px-5 py-2 text-white font-bold text-lg bg-gradient-to-r from-green-400 to-green-600 hover:from-green-500 hover:to-green-700 transition cursor-pointer"
     >
       {Icon && <Icon size={20} />}
       {children}
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,28 @@
+import { Download } from "lucide-react";
+import MainButton from "../atoms/MainButton";
+const handleDownloadClick = () => {
+  alert("test");
+};
+
+export default function Hero() {
+  return (
+    <section className=" py-20 min-h-screen flex flex-col items-center justify-center gap-y-10 text-center">
+      <h1 className="text-2xl md:text-4xl font-bold mb-4">
+        معك <span className="text-[var(--main-color)]">في كل نقرة</span> ...
+        لنحميك من كل خدعة!
+      </h1>
+      <p className="mb-4 md:text-lg ">
+        نحميك من الاحتيال قبل أن يحدث. حارس يرصد ويحلل ويحذّر باستخدام الذكاء
+        الاصطناعي والتنبيهات المجتمعية.
+      </p>
+      <MainButton onClick={handleDownloadClick} icon={Download}>
+        حمّله الآن
+      </MainButton>
+      <img
+        src="../../../public/assets/sections/haris-hero-section.svg"
+        alt="haris-hero-section-image"
+        className="mt-5"
+      />
+    </section>
+  );
+}
